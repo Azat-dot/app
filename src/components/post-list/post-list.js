75fca0c65a5e2ee6a5cda524efcd51dc65721e9b
Azat-dot <azatsafaryan@gmail.com>
2021-07-27 20:49:05 +0500

    props and status

--- src/components/post-list/post-list.js
+++ src/components/post-list/post-list.js
@@ -4,12 +4,21 @@ import PostListItem from '../post-list-item';
 
 import './post-list.css';
 
-const PostList = () => {
+const PostList = ({posts}) => {
+
+    const elements = posts.map((item) => {
+        const {id, ...itemProps} = item;
+        return(
+        <li key={id} className='list-group-item'>
+            <PostListItem {...itemProps}/>  
+            {/* label={item.label} important={item.important}/>можем убрать нижные 2 слоя и написать вместо {...item}/> автомарически */} */}
+        </li>
+       )
+    });
+
     return (
         <ul className="app-list list-group">
-            <PostListItem/>
-            <PostListItem/>
-            <PostListItem/>
+            {elements}
         </ul>
     )
 }
