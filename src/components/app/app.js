01017b2c70bc1c21c26d948eb80abdddbd726050
Azat-dot <azatsafaryan@gmail.com>
2021-08-05 22:32:30 +0500

    add and delete item react

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component} from 'react';
 
 import AppHeader from '../app-header';
 import PostAddForm from '../post-add-form';
@@ -7,26 +7,82 @@ import PostStatusFilter from '../post-status-filter';
 import SearchPanel from '../search-panel';
 
 import './app.css';
+// import style from './App.module.css';
+import styled from 'styled-components';
 
-const App = () => {
+const AppBlock = styled.div `
+        margin: 0 auto;
+        max-width: 800px;
+`;
 
-    const data = [
-        {label: 'First App in React', important: true, id: 1},
-        {label: 'This is hard, but interesting', important: false, id: 2},
-        {label: 'I need a break...', important: false, id: 2 }
-    ]
-    return (
-        <div className="app">
-             <AppHeader/>
-             <div className="search-panel d-flex">
-                <SearchPanel/>
-                <PostStatusFilter/>
-             </div>
-             <PostList posts={data}/>
-             <PostAddForm/>
-        </div>
-       
-    )
+
+export default class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            data : [
+                {label: 'First App in React', important: true, id: 1 },
+                {label: 'This is hard, but interesting', important: false, id: 2 },
+                {label: 'I need a break...', important: false, id: 3 }
+            ]
+        };
+        this.deleteItem = this.deleteItem.bind(this);
+        this.addItem = this.addItem.bind(this);
+
+        this.maxId = 4;
+    }
+
+   
+
+    // state = {
+    //     data : [
+    //         {label: 'First App in React', important: true, id: 1},
+    //         {label: 'This is hard, but interesting', important: false, id: 2},
+    //         {label: 'I need a break...', important: false, id: 3 }
+    //     ]
+    // };
+   
+    deleteItem(id) {
+        this.setState(({data}) => {
+            const index = data.findIndex((elem) => elem.id ===id);
+
+            const newArr = [...data.slice(0, index), ...data.slice(index + 1)];
+           
+            return {
+                data: newArr
+            }
+        });
+    }
+    
+
+    addItem(body) {
+        const newItem = {
+            label: body,
+            important: false,
+            id: this.maxId++
+        }
+        this.setState(({data}) => {
+            const newArr = [...data, newItem];
+            return {
+                data: newArr
+            }
+        });
+    }
+
+    render() {
+        return (
+            <AppBlock>
+                 <AppHeader/>
+                 <div className="search-panel d-flex">
+                    <SearchPanel/>
+                    <PostStatusFilter/>
+                 </div>
+                 <PostList 
+                    posts={this.state.data}
+                    onDelete={this.deleteItem}/>
+                 <PostAddForm
+                    onAdd={this.addItem}/>
+            </AppBlock>
+        )
+    }  
 }
-
-export default App;
--- src/components/post-list/post-list.js
+++ src/components/post-list/post-list.js
@@ -1,27 +1,35 @@
 import React from 'react';
+import {ListGroup} from 'reactstrap';
 
 import PostListItem from '../post-list-item';
 
 import './post-list.css';
 
-const PostList = ({posts}) => {
+const PostList = ({posts, onDelete}) => {
 
-    const elements = posts.map((item) => {
-        const {id, ...itemProps} = item;
-        return(
-        <li key={id} className='list-group-item'>
-            <PostListItem {...itemProps}/>  
-            {/* label={item.label} 
-                important={item.important}
-            />можем убрать нижные 2 слоя и написать вместо {...item}/> */} 
-        </li>
-       )
+    const elements = posts.map( (item) => {
+        // Простой способ проверки на объект + содержится ли в нем информация
+        // if ( typeof item === 'object' && isEmpty(item) ){ 
+            const {id, ...itemProps} = item;
+            return (
+                <li key = {id} className='list-group-item'>
+                    <PostListItem 
+                        {...itemProps}
+                            onDelete={() => onDelete(id)}/>
+                </li>
+            )
+        // }
     });
+    // function isEmpty(obj) {
+    //     for(let key in obj)
+    //     {return true;}
+    //     return false;
+    // }
 
     return (
-        <ul className="app-list list-group">
+        <ListGroup className="app-list">
             {elements}
-        </ul>
+        </ListGroup>
     )
 }
 
